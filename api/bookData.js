fd6bebb18cdd08ea20e0e6f3468cadf6134fe4d7
jakehardin <jakehardin13@gmail.com>
2023-01-21 10:56:47 -0600

    got authors books to show up

--- api/bookData.js
+++ api/bookData.js
@@ -43,10 +43,25 @@ const updateBook = (bookObj) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const booksOnSale = (uid) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/books.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json())
+    .then((data) => {
+      const OnSale = Object.values(data).filter((item) => item.sale);
+      resolve(OnSale);
+    })
+    .catch(reject);
+});
+
 export {
   getBooks,
   createBook,
   deleteBook,
   getSingleBook,
   updateBook,
+  booksOnSale,
 };
